dbacbcefcfea7cb79f0fff97a874443cf097c647
mehikmat <hikmatdhamee@gmail.com>
2020-07-09 13:19:07 +0545

    adding files

--- currencyconverter/src/main/java/microservice/demo/currencyconverter/service/ForexServiceProxy.java
+++ currencyconverter/src/main/java/microservice/demo/currencyconverter/service/ForexServiceProxy.java
@@ -1,13 +1,15 @@
 package microservice.demo.currencyconverter.service;
 
 import microservice.demo.currencyconverter.model.CurrencyConverterBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "forexservice", url = "localhost:8000")
+@FeignClient(name = "forexservice")
+@RibbonClient(name = "forexservice")
 public interface ForexServiceProxy {
 
     @GetMapping("/forexservice/from/{from}/to/{to}")
-    public CurrencyConverterBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+    CurrencyConverterBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
